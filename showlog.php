fe10de676fa4bcbfacf0638814938397293d1229
Nomadic <nomadic.ua@gmail.com>
2014-09-11 14:12:27 +0300

    Reformat log. Minor fixes.

--- showlog.php
+++ showlog.php
@@ -60,56 +60,62 @@ if (!$mysqli->set_charset("utf8")) {
     echo '<div class="alert alert-danger" role="alert">Ошибка при загрузке набора символов UTF8: (' . $mysqli->errno . ') ' . $mysqli->error.'</div>';
 }
 
+// Получение списка пабликов
+if ($source_info = $mysqli->query("SELECT id, name, description FROM source")) {
+    while($row = $source_info->fetch_assoc()) {
+        $sinfo[$row['id']] = array('name'=>$row['name'], 'description'=>$row['description']);
+    }
+    $source_info->close();
+} else {
+    echo '<div class="alert alert-danger" role="alert">Не удалось получить список пабликов: '.$mysqli->error.'</div>';
+}
+
 if (isset($_REQUEST['notnull'])) {
 	$notnull = "AND counter>0";
 } else {
 	$notnull = "";
 }
 if ($source_id == 0) {
-	$select = 'SELECT * FROM updatelog WHERE source_id>? ' . $notnull . ' ORDER BY datetime DESC';
+	$equal = ">";
 } else {
-	$select = 'SELECT * FROM updatelog WHERE source_id=? ' . $notnull . ' ORDER BY datetime DESC';
+	$equal = "=";
 }
 
-if (!($sel_log = $mysqli->prepare($select))) {
-    die("Не удалось подготовить запрос: (" . $mysqli->errno . ") " . $mysqli->error);
+if (!($sel_log = $mysqli->prepare('SELECT * FROM updatelog WHERE source_id' . $equal . '? ' . $notnull . ' ORDER BY datetime DESC'))) {
+    die('<div class="alert alert-dismissible alert-danger" role="alert">Не удалось подготовить запрос: (' . $mysqli->errno . ') ' . $mysqli->error);
 }
 if (!$sel_log->bind_param("i", $source_id)) {
-    die("Не удалось привязать параметры: (" . $sel_log->errno . ") " . $sel_log->error);
+    die('<div class="alert alert-dismissible alert-danger" role="alert">Не удалось привязать параметры: (' . $sel_log->errno . ') ' . $sel_log->error);
 }
-
 if (!$sel_log->execute()) {
-	die('<div class="alert alert-danger" role="alert">Не удалось получить журнал обновлений: (' . $mysqli->errno . ') ' . $mysqli->error.'</div>');
+	die('<div class="alert alert-dismissible alert-danger" role="alert">Не удалось получить журнал обновлений: (' . $mysqli->errno . ') ' . $mysqli->error.'</div>');
 }
 
 $result = $sel_log->get_result();
 
+echo '<table class="table">';
+echo '<thead><tr><th>Дата и время проверки</th><th>Название паблика</th><th>Кол-во нового</th><th>Подробности</th></tr></thead><tbody>';
+
 while ($srow = $result->fetch_assoc()) {
 	$uid = uniqid() . mt_rand();
-	echo '<div class="panel panel-default">	<div class="panel-heading">';
-	echo '<strong>'.$srow['datetime'].'</strong>';
-	if ($source_id == 0) {
-		if ($source_info = $mysqli->query("SELECT * FROM source WHERE id=" . $srow['source_id'])) {
-			$sinfo = $source_info->fetch_assoc();
-			echo ' '.$sinfo['name'].' ('.$sinfo['description'].')';
-			$source_info->close();
-		}
-	}	
-	echo '<span class="badge pull-right">' . $srow['counter'] . '</span>';
-	echo '</div> <div class="panel-body">';
+	echo '<tr><td width="170"><strong>'.$srow['datetime'].'</strong></td><td width="300">';
+    echo ' '.$sinfo[$srow['source_id']]['name'].' ('.$sinfo[$srow['source_id']]['description'].')';
+	echo '</td><td width="20"><center><span class="badge">' . $srow['counter'] . '</span></center></td>';
+    echo '<td>';
 	if ($srow['counter'] > 0) {
 		echo '<button type="button" class="btn btn-info btn-xs" data-toggle="collapse" data-target="#' . $uid . '">Подробности обновления</button>';
 		echo '<div id="' . $uid . '" class="collapse">';
-		echo '<table class="table table-striped">';
+	    echo '<table class="table table-striped table-condensed">';
 		echo $srow['text'];
 		echo '</table>';
 		echo '</div>'; 
 	} else {
 		echo '<span class="label label-default">Обновлений нет</span>';
 	}
-	
-	echo '</div> </div>';
+    echo '</td></tr>';
 }
+
+echo '</tbody></table>';
 
 $sel_log->close();
 $mysqli->close();
